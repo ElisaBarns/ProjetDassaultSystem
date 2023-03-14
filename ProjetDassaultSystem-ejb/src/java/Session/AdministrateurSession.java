3acc324ed4fc3f0a2c68c04e674e4f9b06841bfe
Mel <Mel@192.168.144.133>
2023-03-14 17:12:38 +0100

    Mel, le 14/03/2023, modif sessions admin + Modification Facade utilisateur

--- ProjetDassaultSystem-ejb/src/java/Session/AdministrateurSession.java
+++ ProjetDassaultSystem-ejb/src/java/Session/AdministrateurSession.java
@@ -4,6 +4,13 @@
  */
 package Session;
 
+import Entity.Profil;
+import Entity.Utilisateur;
+import Facade.Piste_opportuniteFacadeLocal;
+import Facade.UtilisateurFacadeLocal;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateful;
 
 /**
@@ -13,6 +20,48 @@ import javax.ejb.Stateful;
 @Stateful
 public class AdministrateurSession implements AdministrateurSessionLocal {
 
+    @EJB
+    private Piste_opportuniteFacadeLocal piste_opportuniteFacade;
+
+    @EJB
+    private UtilisateurFacadeLocal utilisateurFacade;
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    //Creer un utilisateur
+    @Override
+    public void CreerUtilisateur(String l, String mdp, String nom_utilisateur, String prenom_utilisateur, String login_utilisateur, String mdp_utilisateur, String mail_utilisateur, String tel_utilisateur, boolean inactif, Date date_creation_utilisateur, Date date_inactivation_utilisateur, Date date_modification_utilisateur) {
+        Utilisateur u = null;
+        u = utilisateurFacade.Authentification(l,mdp);
+        if(u!=null)
+        {
+            List <Profil> listeProfils =u.getLesProfils();
+            int i=0;
+            while(i<=listeProfils.size())
+            {
+                if(listeProfils.get(i).getFonction().equals(administrateur))
+                {
+                    utilisateurFacade.CreerUtilisateur(String nom_utilisateur, String prenom_utilisateur, String login_utilisateur, String mdp_utilisateur, String mail_utilisateur, String tel_utilisateur, boolean inactif, Date date_creation_utilisateur, Date date_inactivation_utilisateur, Date date_modification_utilisateur);
+                }
+                else
+                {
+                System.out.println ("Vous n'êtes pas administrateur");
+                }
+                i++;
+            }
+        }
+    }
+
+    //Modifier un utilisateur
+    @Override
+    public void ModifierUtilisateur() {
+    }
+
+    //Supprimer/Inactiver un utilisateur
+    @Override
+    public void InactiverUtilisateur() {
+    }
+    
+    
 }
--- ProjetDassaultSystem-ejb/src/java/Session/AdministrateurSessionLocal.java
+++ ProjetDassaultSystem-ejb/src/java/Session/AdministrateurSessionLocal.java
@@ -4,6 +4,7 @@
  */
 package Session;
 
+import java.util.Date;
 import javax.ejb.Local;
 
 /**
@@ -12,5 +13,11 @@ import javax.ejb.Local;
  */
 @Local
 public interface AdministrateurSessionLocal {
+
+    void CreerUtilisateur(String l, String mdp, String nom_utilisateur, String prenom_utilisateur, String login_utilisateur, String mdp_utilisateur, String mail_utilisateur, String tel_utilisateur, boolean inactif, Date date_creation_utilisateur, Date date_inactivation_utilisateur, Date date_modification_utilisateur);
+
+    void ModifierUtilisateur();
+
+    void InactiverUtilisateur();
     
 }

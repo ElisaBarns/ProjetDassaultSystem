88c01f9009e0bf79eafa425d5505f7300d3ac9e0
marie <marie@DESKTOP-JVKA550>
2023-03-20 14:20:29 +0100

    Marie avant authentification session

---
+++ ProjetDassaultSystem-ejb/src/java/Session/UtilisateurSession.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatefulEjbClass.java to edit this template
+ */
+package Session;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author marie
+ */
+@Stateful
+public class UtilisateurSession implements UtilisateurSessionLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
